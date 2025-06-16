fe695351059b694582568ee6790c86a93ce80f20
karrar3139 <karrarkp3139@gmail.com>
2025-06-17 00:27:14 +0300

    question 2

--- source.py
+++ source.py
@@ -4,13 +4,13 @@ wb = load_workbook('sagatave_eksamenam.xlsx')
 ws = wb['Lapa_0']
 max_row = ws.max_row
 
-
 count = 0
-for row in range(2, max_row + 1):
-    address = ws['D' + str(row)].value
-    number = ws['L' + str(row)].value
 
-    if isinstance(address, str) and address.startswith('Ain') and isinstance(number, (int, float)) and number < 40:
+for row in range(2, max_row + 1):
+    priority = ws['H' + str(row)].value
+    delivery_date = ws['J' + str(row)].value
+
+    if priority == 'High' and hasattr(delivery_date, 'year') and delivery_date.year == 2015:
         count += 1
 
 print(count)
